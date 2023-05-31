ad3fc20e6c053490cd72f6f8011e063b57641bcd
Дмитрий Тяпин <tyapin2002@mail.ru>
2023-05-31 17:38:09 +0400

    фикс с комментарием

--- EducationMauiApp/App.xaml.cs
+++ EducationMauiApp/App.xaml.cs
@@ -8,7 +8,13 @@ public partial class App : Application
     public App()
 	{
 		InitializeComponent();
-		GraphLayoutViewModel = (GraphLayoutViewModel)Current.Resources[nameof(GraphLayoutViewModel)];
+
+        // !!! В данном случае, присваивание значения - необходимость, обусловленная тем, что при создании приложения на Android
+        //	при инициализации GraphLayoutViewModel выбрасывается ошибка
+        //		System.TypeInitializationException: 'The type initializer for 'EducationMauiApp.App' threw an exception.',
+        //	связанная со статическим конструктором.
+        GraphLayoutViewModel = (GraphLayoutViewModel)Current.Resources[nameof(GraphLayoutViewModel)];
+
 		MainPage = new AppShell();
 	}
 }
